47b9e050315ece98e0d02befaa8182132bd4d319
MScholtes <markusscholtes@hotmail.com>
2020-05-03 09:35:01 +0200

    Update to V1.1.2

--- Module/functions/Split-File.ps1
+++ Module/functions/Split-File.ps1
@@ -19,31 +19,65 @@ Split-File "BigFile.dat" 10000000
 Divides the file BigFile.dat into parts of 10000000 byte size
 .Notes
 Author: Markus Scholtes
-Version: 1.0
-Date: 2017-09-04
+Version: 1.01
+Date: 2020-05-03
 #>
-function Split-File([STRING] $Path, [int] $Newsize = 100MB)
+function Split-File([STRING] $Path, [INT64] $Newsize = 100MB)
 {
+	if ($Newsize -le 0)
+	{
+		Write-Error "Only positive sizes allowed"
+		return
+	}
+
 	"Splitting file $Path to parts of $Newsize byte size"
 	$FILEPATH = [IO.Path]::GetDirectoryName($Path)
 	if ($FILEPATH -ne "") { $FILEPATH = $FILEPATH + "\" }
 	$FILENAME = [IO.Path]::GetFileNameWithoutExtension($Path)
 	$EXTENSION  = [IO.Path]::GetExtension($Path)
-	$OBJREADER = New-Object System.IO.BinaryReader([System.IO.File]::Open($Path, [System.IO.FileMode]::Open, [System.IO.FileAccess]::Read))
-	[Byte[]]$BUFFER = New-Object Byte[] $Newsize
-	[int]$BYTESREAD = 0
 
+	$MAXVALUE = 1GB # Hard maximum limit for Byte array for 64-Bit .Net 4 = [INT32]::MaxValue - 56, see here https://stackoverflow.com/questions/3944320/maximum-length-of-byte
+	# but only around 1.5 GB in 32-Bit environment! So I chose 1 GB just to be safe
+	$PASSES = [MATH]::Floor($Newsize / $MAXVALUE)
+	$REMAINDER = $Newsize % $MAXVALUE
+	if ($PASSES -gt 0) { $BUFSIZE = $MAXVALUE } else { $BUFSIZE = $REMAINDER }
+
+	$OBJREADER = New-Object System.IO.BinaryReader([System.IO.File]::Open($Path, [System.IO.FileMode]::Open, [System.IO.FileAccess]::Read))
+	[Byte[]]$BUFFER = New-Object Byte[] $BUFSIZE
 	$NUMFILE = 1
-	while (($BYTESREAD = $OBJREADER.Read($BUFFER, 0, $BUFFER.Length)) -gt 0)
-	{
-		"Reading $BYTESREAD bytes of $Path"
+
+	do {
 		$NEWNAME = "{0}{1}{2,2:00}{3}" -f ($FILEPATH, $FILENAME, $NUMFILE, $EXTENSION)
-		$OBJWRITER = New-Object System.IO.BinaryWriter([System.IO.File]::Create($NEWNAME))
-		"Writing file $NEWNAME"
-		$OBJWRITER.Write($BUFFER, 0, $BYTESREAD)
-		$OBJWRITER.Close()
-	  ++$NUMFILE
-	}
+
+		$COUNT = 0
+		$OBJWRITER = $NULL
+		[INT32]$BYTESREAD = 0
+		while (($COUNT -lt $PASSES) -and (($BYTESREAD = $OBJREADER.Read($BUFFER, 0, $BUFFER.Length)) -gt 0))
+		{
+			if (!$OBJWRITER)
+			{
+				$OBJWRITER = New-Object System.IO.BinaryWriter([System.IO.File]::Create($NEWNAME))
+				"Writing to file $NEWNAME"
+			}
+			"Reading $BYTESREAD bytes of $Path"
+			$OBJWRITER.Write($BUFFER, 0, $BYTESREAD)
+			$COUNT++
+		}
+		if (($REMAINDER -gt 0) -and (($BYTESREAD = $OBJREADER.Read($BUFFER, 0, $REMAINDER)) -gt 0))
+		{
+			if (!$OBJWRITER)
+			{
+				$OBJWRITER = New-Object System.IO.BinaryWriter([System.IO.File]::Create($NEWNAME))
+				"Writing to file $NEWNAME"
+			}
+			"Reading $BYTESREAD bytes of $Path"
+			$OBJWRITER.Write($BUFFER, 0, $BYTESREAD)
+		}
+
+		if ($OBJWRITER) { $OBJWRITER.Close() }
+		++$NUMFILE
+	} while ($BYTESREAD -gt 0)
+
 	$OBJREADER.Close()
 }
 
@@ -96,7 +130,7 @@ function Join-File([STRING] $Path)
 		$OBJREADER.BaseStream.CopyTo($OBJWRITER.BaseStream)
 		$OBJWRITER.BaseStream.Flush()
 
-  	$OBJREADER.Close()
+		$OBJREADER.Close()
 	}
 
 	$OBJWRITER.Close()
